16e42931b865c01316fa65d22c665026946b493f
josehidalgoc-cpu <josehidalgoc@uees.edu.ec>
2026-07-29 23:52:54 -0500

    Final

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,56 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement; // <-- IMPORTANTE: Necesario para cambiar de escena
 
 public class PlayerController : MonoBehaviour
 {
     public float playerJumpForce = 20f;
-    public float playerSpeed = 7f;
+    public float playerSpeed = 5f;
     public Sprite[] mySprites;
     private int index = 0;
 
     private Rigidbody2D myrigidbody2D;
     private SpriteRenderer mySpriteRenderer;
-    //public GameObject Bullet;
-    //public GameManager myGameManager;
+    public GameObject Bullet;
 
     void Start()
     {
         myrigidbody2D = GetComponent<Rigidbody2D>();
         mySpriteRenderer = GetComponent<SpriteRenderer>();
-        StartCoroutine(WalkCoroutine()); 
-      // myGameManager = FindObjectOfType<GameManager>();
+        
+        if (mySprites != null && mySprites.Length > 0)
+        {
+            StartCoroutine(WalkCoRutine());
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            myrigidbody2D.linearVelocity = new Vector2(myrigidbody2D.linearVelocity.x, playerJumpForce);
-        }
+        // Nota: Si usas Unity 2022 o inferior, cambia linearVelocity por velocity
         myrigidbody2D.linearVelocity = new Vector2(playerSpeed, myrigidbody2D.linearVelocity.y);
         
-        //if(Input.GetKeyDown(KeyCode.E))
-        //{
-          // Instantiate(Bullet, transform.position, Quaternion.identity);
-        //}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Mantiene la velocidad horizontal mientras salta
+            myrigidbody2D.linearVelocity = new Vector2(playerSpeed, playerJumpForce);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+{
+    // Cambiamos 'transform.position' por esta posición fija más adelante
+    Instantiate(Bullet, transform.position + new Vector3(2f, 0, 0), Quaternion.identity);
+}
     }
 
-    // CORREGIDO: Se escribió correctamente 'WalkCoroutine'
-    IEnumerator WalkCoroutine()
+    IEnumerator WalkCoRutine()
     {
-        while (true)
+        yield return new WaitForSeconds(0.08f);
+
+        if (mySprites != null && mySprites.Length > 0)
         {
-            yield return new WaitForSeconds(0.05f);
             mySpriteRenderer.sprite = mySprites[index];
             index++;
-            
-            if (index == 5)
+
+            if (index >= mySprites.Length)
             {
                 index = 0;
             }
         }
+        
+        StartCoroutine(WalkCoRutine());
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ItemGood"))
+        {
+            Debug.Log("¡Has recogido un corazón!");
+            Destroy(collision.gameObject);
+        }
+        else if (collision.CompareTag("ItemBad"))
+        {
+            Debug.Log("¡Has chocado con un obstáculo!");
+            Destroy(collision.gameObject);
+            PlayerDeath();
+        }
+        else if (collision.CompareTag("DeathZone"))
+        {
+            Debug.Log("¡Has caído en la zona de muerte!");
+            PlayerDeath();
+        }
+    }
+
+    void PlayerDeath()
+    {
+        SceneManager.LoadScene("GameOver"); // <-- CORREGIDO AQUÍ
     }
 }
